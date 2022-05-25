63fe96e4a1dadd49fae7581b1df1b6b7efe9ad6e
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-05-25 23:37:05 +0100

    mvp met

--- bin2decapp/src/App.js
+++ bin2decapp/src/App.js
@@ -6,6 +6,25 @@ function App() {
    //if input contains only 1's and zeros, then it is binary
    const isBinary = new RegExp("^[1/0]+$").test(input);
    const error = isBinary ? null : "Binary numbers only";
+   const result = binaryToDec(input);
+
+   function binaryToDec(string) {
+      let strIndex = [];
+      let sum = 0;
+
+      for (let i = 0; i < string.length; i++) {
+         strIndex.push(i);
+      }
+      const reversed = strIndex.reverse();
+
+      for (let i = 0; i < reversed.length; i++) {
+         if (string[i] !== "0") {
+            sum += 2 ** reversed[i];
+         }
+      }
+      return sum;
+   }
+
    function handleChange(event) {
       setInput(event.target.value);
    }
@@ -13,7 +32,7 @@ function App() {
       <div className="App">
          <h1>Binary to Decimal Converter</h1>
          <input type="text" onChange={handleChange}></input>
-         {isBinary && <p>{input}</p>}
+         {isBinary && <p>{result}</p>}
          <div>
             <p>{error}</p>
          </div>
