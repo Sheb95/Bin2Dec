03161b71ebcd3d7d70bd1ad4ad92210edbdf3032
Sheb95 <80277508+Sheb95@users.noreply.github.com>
2022-05-25 23:12:55 +0100

    check for binary done

--- bin2decapp/src/App.js
+++ bin2decapp/src/App.js
@@ -1,11 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+   let [input, setInput] = useState("");
+   //if input contains only 1's and zeros, then it is binary
+   const isBinary = new RegExp("^[1/0]+$").test(input);
+   const error = isBinary ? null : "Binary numbers only";
+   function handleChange(event) {
+      setInput(event.target.value);
+   }
    return (
       <div className="App">
          <h1>Binary to Decimal Converter</h1>
-         <input></input>
+         <input type="text" onChange={handleChange}></input>
+         {isBinary && <p>{input}</p>}
+         <div>
+            <p>{error}</p>
+         </div>
       </div>
    );
 }
